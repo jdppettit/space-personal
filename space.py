5d88d9a376a2f640a81421919d9a33985b966645
Joe Pettit <jpettit@pettitservers.com>
2015-02-04 20:45:03 -0600

    Actually corrected issue with index not loading

--- space.py
+++ space.py
@@ -208,7 +208,10 @@ def index():
     log = get_log_datelevel(date="day", level=3)
     images = get_all_images()
     stats = get_host_statistic_specific(1)
-
+    try:
+        servers[0]
+    except:
+        servers = None
     return render_template("index.html", servers = servers, images=images, log=log, stats=stats)
 
 @app.route('/create', methods=['POST'])
