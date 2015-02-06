5afd7f8196a2ccca22943dc20857088426181ec0
Joe Pettit <jpettit@pettitservers.com>
2015-02-05 22:51:26 -0600

    Updates to setup.sh, moving closer to 100% automated there. Removed old requirements from requirements.txt

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,10 +6,23 @@ echo -e "This script will attempt to start and install all system services neces
 echo -n
 echo -e "Updating yum db..."
 
+echo -n
+echo -e "Adding mongodb.repo to /etc/yum.repos.d"
+
+cp /srv/space/conf.d/mongodb.repo /etc/yum.repos.d/mongodb.repo
+
+echo -n
+echo -e "Getting RPM for RabbitMQ..."
+
+wget -O /etc/yum.repos.d/epel-erlang.repo http://repos.fedorapeople.org/repos/peter/erlang/epel-erlang.repo
+yum install erlang
+rpm --import http://www.rabbitmq.com/rabbitmq-signing-key-public.asc
+rpm -Uvh http://www.rabbitmq.com/releases/rabbitmq-server/v3.1.4/rabbitmq-server-3.1.4-1.noarch.rpm
+
 yum update
 
 echo -n
-echo -e "Installing mongodb-server rabbitmq-server qemu-kvm qemu-img virt-manager libvirt libvirt-python python virtinst libvirt-client virt-install virt-viewer dhcp"
+echo -e "Installing mongodb-org rabbitmq-server qemu-kvm qemu-img virt-manager libvirt libvirt-python python virtinst libvirt-client virt-install virt-viewer dhcp"
 
 for package in mongodb-server rabbitmq-server qemu-kvm qemu-img virt-manager libvirt libvirt-python python virtinst libvirt-client virt-install virt-viewer dhcp python-pip
 do
@@ -59,14 +72,24 @@ echo -e "Starting MongoDB..."
 service mongod start
 
 echo -n
+echo -e "Starting libvirtd..."
+
+service libvirtd start
+
+echo -n
+echo -e "Starting RabbitMQ..."
+
+service rabbitmq-server start
+
+echo -n
 echo -e "Starting Space..."
+
+service space start
 
 echo -n
 echo -e "Starting celery worker..."
 
 sh /srv/space/scripts/celery.sh
-
-service space start
 
 echo -n
 echo -e "----------------------------------------"
